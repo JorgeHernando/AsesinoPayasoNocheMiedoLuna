d06db454dd594118f6d8bac9051bf04adc80bbb2
JorgeHernando <jorge.hernando@enti.cat>
2023-02-27 19:20:20 +0100

    Prototipo

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Score : MonoBehaviour
+{
+    private GameObject[] activeNPC;
+
+    public void Update()
+    {
+        activeNPC = GameObject.FindGameObjectsWithTag("NPCAlive");
+
+        if (activeNPC.Length == 0)
+        {
+            Victory();
+        }
+    }
+
+    public void Victory()
+    {
+        if (activeNPC.Length == 0)
+        {
+            SceneManager.LoadScene("WonScene");
+        }
+    }
+}

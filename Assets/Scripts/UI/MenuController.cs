a1602aac010efa57c721118c8467f8cfabd85b39
JorgeHernando <jorge.hernando@enti.cat>
2023-04-16 19:06:38 +0200

    cositas main menu

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -9,11 +9,13 @@ public class MenuController : MonoBehaviour
     [SerializeField] Animator animFade;
     [SerializeField] Animator startUp;
     [SerializeField] Animator handDown;
+    [Space]
+    [SerializeField] GameObject optionsPanel;
+    [SerializeField] GameObject creditsPanel;
+    [SerializeField] GameObject menuPanel;
 
-    public void Credits()
-    {
-        SceneManager.LoadScene("Credits");
-    }
+    private bool isOptionsActive = false;
+    private bool isCreditsActive = false;
 
     public void ButtonUp()
     {
@@ -31,6 +33,35 @@ public class MenuController : MonoBehaviour
         animFade.SetTrigger("FadeIn");
     }
 
+    public void Options()
+    {
+        optionsPanel.SetActive(true);
+        menuPanel.SetActive(false);
+        isOptionsActive = true;
+    }
+    
+    public void Credits()
+    {
+        creditsPanel.SetActive(true);
+        menuPanel.SetActive(false);
+        isCreditsActive = true;
+    }
+
+    public void GoBack()
+    {
+        if(isOptionsActive == true)
+        {
+            optionsPanel.SetActive(false);
+            menuPanel.SetActive(true);
+            isOptionsActive = false;
+        }
+        else if (isCreditsActive == true)
+        {
+            creditsPanel.SetActive(false);
+            menuPanel.SetActive(true);
+            isCreditsActive = false;
+        }
+    }
     public void LoadFirstLevel()
     {
         SceneManager.LoadScene("InGame");
@@ -38,6 +69,7 @@ public class MenuController : MonoBehaviour
 
     public void Exit()
     {
+        Debug.Log("Saliste del juego");
         Application.Quit();
     }
 }

6fed7e7e54ab1156fa1c20ce7919197f8ac4163e
PC-imatge-2022-2023 <licenses@enti.cat>
2023-05-16 10:04:48 +0200

    Mapa y demás

--- Assets/_DiscoKilla/Scripts/SlimeBehaviour/BulletSpawn.cs
+++ Assets/_DiscoKilla/Scripts/SlimeBehaviour/BulletSpawn.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class BulletSpawn : MonoBehaviour
 {
+    public float firerate;
+    public float nextfire;
     [SerializeField] GameObject prefabBullet;
     [SerializeField] Vector3 posBullet;
 
     public void SpawnBullet()
     {
-        if (prefabBullet != null)
+        if( Time.time > firerate)
         {
+            nextfire = Time.time + firerate;
+            Debug.Log(nextfire);
             GameObject bulletPrefab = Instantiate(prefabBullet, this.transform.position + posBullet, Quaternion.identity);
             Destroy(bulletPrefab, 5);
             //prefabParticles
+
         }
     }
 }
